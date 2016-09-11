2c88f9b38704d81494a5291a8d49f0dbc7ad18be
Gable Heng <gable@kaliberlabs.com>
2016-09-10 21:57:03 -0700

    commit

--- app/static/js/maps.js
+++ app/static/js/maps.js
@@ -23,55 +23,93 @@ $(document).ready(function(){
     });
     var behavior = new H.mapevents.Behavior(mapEvents);
 
+//GEOCODE
+    // Create the parameters for the geocoding request:
+   var geocodingParams = {
+     searchText: 'San Francisco'
+   };
 
-        // Create a group object to hold map markers:
-    var group = new H.map.Group();
+    // Define a callback function to process the geocoding response:
+    var onGeoCodeResult = function(result) {
+      var results = []
+      var locations = result.Response.View[0].Result,
+        position,
+        marker;
+      // Add a marker for each location found
+      for (i = 0;  i < locations.length; i++) {
+          position = {
+            lat: locations[i].Location.DisplayPosition.Latitude,
+            lng: locations[i].Location.DisplayPosition.Longitude
+          };
+          search(position)
+          console.log(position)
+      }
 
-    // Create the default UI components:
-    var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
-
-    // Add the group object to the map:
-    map.addObject(group);
-
-    // Obtain a Search object through which to submit search
-    // requests:
-    var search = new H.places.Search(platform.getPlacesService()),
-      searchResult, error;
-
-    // Define search parameters:
-    var params = {
-    // Plain text search for places with the word "hotel"
-    // associated with them:
-      'q': 'hotel',
-    //  Search in the Chinatown district in San Francisco:
-      'at': '37.7942,-122.4070'
+      return results
     };
 
-    // Define a callback function to handle data on success:
-    function onResult(data) {
-      addPlacesToMap(data.results);
-    }
+    // Get an instance of the geocoding service:
+    var geocoder = platform.getGeocodingService();
 
-    // Define a callback function to handle errors:
-    function onError(data) {
-      error = data;
-    }
+    // Call the geocode method with the geocoding parameters,
+    // the callback and an error callback function (called if a
+    // communication error occurs):
+    var coords = geocoder.geocode(geocodingParams, onGeoCodeResult, function(e) {
+      alert(e);
+    });
+//ENDGEOCODE
+    function search(coords) {
+    //SEARCH
+            // Create a group object to hold map markers:
+        var group = new H.map.Group();
 
-    // This function adds markers to the map, indicating each of
-    // the located places:
-    function addPlacesToMap(result) {
-      group.addObjects(result.items.map(function (place) {
-      var marker = new H.map.Marker({lat: place.position[0],
-        lng: place.position[1]})
-        
-      marker.title = "DSFsdfsd"
-      return marker;
-      }));
-    }
+        // Create the default UI components:
+        var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
 
-    // Run a search request with parameters, headers (empty), and
-    // callback functions:
-    search.request(params, {}, onResult, onError);
+        // Add the group object to the map:
+        map.addObject(group);
+
+        // Obtain a Search object through which to submit search
+        // requests:
+        var search = new H.places.Search(platform.getPlacesService()),
+          searchResult, error;
+
+        // Define search parameters:
+        var params = {
+        // Plain text search for places with the word "hotel"
+        // associated with them:
+          'q': 'hotel',
+        //  Search in the Chinatown district in San Francisco:
+          'at': coords.lat+',' + coords.long
+        };
+        // Define a callback function to handle data on success:
+        function onSearchResult(data) {
+          addPlacesToMap(data.results);
+        }
+
+        // Define a callback function to handle errors:
+        function onError(data) {
+          error = data;
+        }
+
+        // This function adds markers to the map, indicating each of
+        // the located places:
+        function addPlacesToMap(result) {
+          group.addObjects(result.items.map(function (place) {
+          var marker = new H.map.Marker({lat: place.position[0],
+            lng: place.position[1]})
+
+          marker.title = "DSFsdfsd"
+          return marker;
+          }));
+        }
+
+        // Run a search request with parameters, headers (empty), and
+        // callback functions:
+        search.request(params, {}, onSearchResult, onError);
+    }
+//ENDSEARCH
+
 
 
 
